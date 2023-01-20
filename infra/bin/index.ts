f1c1ff9b7ec3ce58e8225a4b8b00820c06e81723
Fabiola Ciezza <jafettayala@gmail.com>
2023-01-19 21:13:25 -0500

    cleanup PipelineStack part 1

--- infra/bin/index.ts
+++ infra/bin/index.ts
@@ -3,13 +3,13 @@ import 'source-map-support/register'
 import { App } from 'aws-cdk-lib'
 import { FooStack } from '../stacks'
 
-// ;(async () => {
-//   new FooStack(
-//     new App(),
-//     'Foo-Api',
-//     {
-//       stackName: 'Foo-Api',
-//       description: `Resources for Foo Service`,
-//     }
-//   )
-// })()
+(async () => {
+  new FooStack(
+    new App(),
+    'Restaurant-API',
+    {
+      stackName: 'Restaurant-Api',
+      description: `Resources for Restaurant-Api Service`,
+    }
+  )
+})()
